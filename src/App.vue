acb67936178b3c484cde7c309cfc9a7a90b24643
venzel <eneas.eng@yahoo.com>
2022-03-05 18:14:32 -0300

    feat: setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Task />
+    </div>
+</template>
+
+<script>
+import Task from './views/Task.vue';
+
+export default {
+    components: { Task },
+};
+</script>
+
+<style></style>
---
+++ src/views/Task.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <ul v-if="taskFilter.length">
+            <li v-for="task in taskFilter" :key="task.id">{{ task.title }}</li>
+        </ul>
+        <p v-else>Não existe tarefas ativas!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tasks: [
+                { id: 1, title: 'Caminhar', status: false },
+                { id: 2, title: 'Malhar', status: true },
+                { id: 3, title: 'Estudar', status: false },
+                { id: 4, title: 'Trabalhar', status: false },
+            ],
+        };
+    },
+    computed: {
+        taskFilter() {
+            return this.tasks.filter((e) => e.status);
+        },
+    },
+};
+</script>
+
+<style></style>
